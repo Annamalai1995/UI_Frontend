1e423a656693e41876f7cdec1fc43827ee3084c1
Annamalai1995 <s.annamalai1995@gmail.com>
2025-06-27 12:54:47 +0530

    bs and js

---
+++ JavaScript/function.js
@@ -0,0 +1,14 @@
+function Muthayammal()
+{
+    let name="Annamalai"
+    alert("MY name is :"+name)
+}
+let Action=(value,value1)=>
+{
+    let result=value*value1
+    document.writeln("The value is:"+value+"The value 1:"+value1+"The Result"+result)
+}
+const display=()=>
+{
+    console.log('HELLLOOO')
+}
